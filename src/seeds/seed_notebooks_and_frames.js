7a53ce94375599ca7d3380f78184c6cf9a5c43e4
Auto <>
2016-02-28 10:14:50 +1100

    Added seeder

---
+++ src/seeds/seed_notebooks_and_frames.js
@@ -0,0 +1,31 @@
+const notebooks = [
+  {
+    id: 1,
+    title: 'My notebook'
+  }
+];
+
+const frames = [
+  {
+    id: 1,
+    notebookId: 1,
+    title: 'A frame',
+    content: 'Enough content to keep you content.'
+  },
+  {
+    id: 2,
+    notebookId: 1,
+    title: 'Another frame',
+    content: 'Why so many frames?'
+  }
+];
+
+exports.seed = function(knex, Promise) {
+  return (knex('frames').del()
+    .then(() => knex('notebooks').del())
+    .then(() => Promise.all([
+      ...notebooks.map((notebook) => knex('notebooks').insert(notebook)),
+      ...frames.map((frame) => knex('frames').insert(frame))
+    ]))
+  );
+};

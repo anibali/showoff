d79cbe47556a60564c7cc6afd683e3e33084b14d
Aiden Nibali <dismaldenizen@gmail.com>
2016-03-01 14:52:30 +1100

    Fix seed race case, nice overflow behaviour for frames

--- src/seeds/seed_notebooks_and_frames.js
+++ src/seeds/seed_notebooks_and_frames.js
@@ -32,31 +32,24 @@ const frames = [
       type: 'vega',
       body: {
         "width": 400,
-        "height": 200,
-        "padding": {"top": 10, "left": 30, "bottom": 30, "right": 10},
+        "height": 270,
         "data": [
           {
             "name": "table",
             "values": [
               {"x": 1,  "y": 28}, {"x": 2,  "y": 55},
               {"x": 3,  "y": 43}, {"x": 4,  "y": 91},
-              {"x": 5,  "y": 81}, {"x": 6,  "y": 53},
-              {"x": 7,  "y": 19}, {"x": 8,  "y": 87},
-              {"x": 9,  "y": 52}, {"x": 10, "y": 48},
-              {"x": 11, "y": 24}, {"x": 12, "y": 49},
-              {"x": 13, "y": 87}, {"x": 14, "y": 66},
-              {"x": 15, "y": 17}, {"x": 16, "y": 27},
-              {"x": 17, "y": 68}, {"x": 18, "y": 16},
-              {"x": 19, "y": 49}, {"x": 20, "y": 15}
+              {"x": 5,  "y": 81}, {"x": 6,  "y": 53}
             ]
           }
         ],
         "scales": [
           {
             "name": "x",
-            "type": "ordinal",
+            "type": "linear",
             "range": "width",
-            "domain": {"data": "table", "field": "x"}
+            "domain": {"data": "table", "field": "x"},
+            "nice": true
           },
           {
             "name": "y",
@@ -72,20 +65,24 @@ const frames = [
         ],
         "marks": [
           {
-            "type": "rect",
+            "type": "line",
             "from": {"data": "table"},
             "properties": {
               "enter": {
                 "x": {"scale": "x", "field": "x"},
-                "width": {"scale": "x", "band": true, "offset": -1},
                 "y": {"scale": "y", "field": "y"},
-                "y2": {"scale": "y", "value": 0}
-              },
-              "update": {
+                "stroke": {"value": "steelblue"}
+              }
+            }
+          },
+          {
+            "type": "symbol",
+            "from": {"data": "table"},
+            "properties": {
+              "enter": {
+                "x": {"scale": "x", "field": "x"},
+                "y": {"scale": "y", "field": "y"},
                 "fill": {"value": "steelblue"}
-              },
-              "hover": {
-                "fill": {"value": "red"}
               }
             }
           }
@@ -98,9 +95,7 @@ const frames = [
 exports.seed = function(knex, Promise) {
   return (knex('frames').del()
     .then(() => knex('notebooks').del())
-    .then(() => Promise.all([
-      ...notebooks.map((notebook) => knex('notebooks').insert(notebook)),
-      ...frames.map((frame) => knex('frames').insert(frame))
-    ]))
+    .then(() => Promise.all(notebooks.map((notebook) => knex('notebooks').insert(notebook))))
+    .then(() => Promise.all(frames.map((frame) => knex('frames').insert(frame))))
   );
 };

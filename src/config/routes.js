cb67c8d027ff70655063fe3737e6a8d5e1ef5d8a
Aiden Nibali <dismaldenizen@gmail.com>
2017-03-17 12:12:45 +1100

    Added API v2

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,9 +1,12 @@
-const api = require('../controllers/api');
+const apiv1 = require('../controllers/api_v1');
+const apiv2 = require('../controllers/api_v2');
 
 const routes = {};
 
 routes.connect = (app) => {
-  app.use('/api', api);
+  app.use('/api', apiv1);
+  app.use('/api/v1', apiv1);
+  app.use('/api/v2', apiv2);
 };
 
 module.exports = routes;

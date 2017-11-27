93dfc1d22f92448dabc02dd24ae3f8f8934ea1c0
Aiden Nibali <dismaldenizen@gmail.com>
2017-11-27 20:04:36 +1100

    Retire API v1

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,7 +1,6 @@
 import passport from 'passport';
 
 import authController from '../controllers/authController';
-import apiv1 from '../controllers/api/v1';
 import apiv2 from '../controllers/api/v2';
 
 
@@ -18,8 +17,6 @@ const authenticate = (req, res, next) => {
 
 routes.connect = (app) => {
   app.use('/auth', authController);
-  app.use('/api', authenticate, apiv1);
-  app.use('/api/v1', authenticate, apiv1);
   app.use('/api/v2', authenticate, apiv2);
 };
 

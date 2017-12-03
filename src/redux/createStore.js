99809749690e411e7b88282dfa6c79cbb9ce0af8
Aiden Nibali <dismaldenizen@gmail.com>
2017-12-03 11:06:47 +1100

    Add hook for the Redux browser extension in dev mode

--- src/redux/createStore.js
+++ src/redux/createStore.js
@@ -9,10 +9,16 @@ import rootSaga from './sagas/rootSaga';
 export default (initialState) => {
   const sagaMiddleware = createSagaMiddleware();
 
+  let composeEnhancers = Redux.compose;
+  if(process.env.IN_BROWSER && process.env.NODE_ENV === 'development') {
+    // eslint-disable-next-line no-underscore-dangle
+    composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || Redux.compose;
+  }
+
   const store = Redux.createStore(
     combinedReducers,
     initialState,
-    Redux.applyMiddleware(ReduxThunk, sagaMiddleware));
+    composeEnhancers(Redux.applyMiddleware(ReduxThunk, sagaMiddleware)));
 
   sagaMiddleware.run(rootSaga);
 
